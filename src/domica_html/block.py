6261bc658d0283e4914e2eb3527d04b28faa067c
opolonix <nefris.me@gmail.com>
2026-03-18 13:23:16 +0300

    добавил возможность указывать сепаратор для text ноды

--- src/domica_html/block.py
+++ src/domica_html/block.py
@@ -5,25 +5,29 @@ from .inctement import inc
 
 
 class text(node_container):
-    indent_prefix: bool = False
+    indent_prefix = ""
 
     def __init__(
         self,
         *value: Union[str, node],
-        anchor = True
+        anchor: bool = True,
+        sep: str = ""
     ):
         self.value = value
+        self.sep = sep
         super().__init__(anchor)
 
     async def render(self):
-        content = await self.render_item(self.value)
+        sep = await self.render_item(sep)
+        content = sep.join([await self.render_item(v) for v in self.value])
+
         if self.children:
             with inc:
                 content += await self.render_item(self.children)
 
         if self.indent_prefix:
-            return inc.enter_space + content
+            return await self.render_item(self.indent_prefix) + content
         return content
 
 class line(text):
-    indent_prefix: bool = True
+    indent_prefix = inc.enter_space
